46b5a8d1da1cdf5674b3498bdefa47d5979024fb
czarnini <boguckimi@gmail.com>
2017-09-26 16:06:38 +0200

    Changed listview to recyclerview

--- app/src/main/java/com/bogucki/router/activities/Clients.java
+++ app/src/main/java/com/bogucki/router/activities/Clients.java
@@ -2,16 +2,19 @@ package com.bogucki.router.activities;
 
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bogucki.router.R;
@@ -19,83 +22,82 @@ import com.bogucki.router.Utils.ConstantValues;
 import com.bogucki.router.dialogs.AddNewOrEditClientDialog;
 import com.bogucki.router.dialogs.ChooseActionForClientDialog;
 import com.bogucki.router.models.Client;
-import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Clients extends AppCompatActivity
-{
+public class Clients extends AppCompatActivity {
 
-    private ListView clientsList;
+    private RecyclerView clientsList;
     private static final String TAG = Clients.class.getSimpleName();
     private DatabaseReference databaseReference;
-    FirebaseListAdapter<Client> mAdapter;
+    FirebaseRecyclerAdapter mAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clients);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         attachFireBaseAdapter();
-        attachOnListItemClickListener();
         handleFloatingButton();
     }
 
+    //TODO -> divider 80dp transparent
     private void attachFireBaseAdapter() {
         databaseReference = FirebaseDatabase.getInstance().getReference().child(ConstantValues.CLIENTS_FIREBASE);
-        clientsList = (ListView) findViewById(R.id.clients_list);
-        mAdapter = new FirebaseListAdapter<Client>(this, Client.class, R.layout.client_list_item, databaseReference) {
-            @Override
-            protected void populateView(View view, Client client, int position) {
-                TextView name = (TextView) view.findViewById(R.id.client_name);
-                TextView address = (TextView) view.findViewById(R.id.client_address);
-                TextView pushTV = (TextView) view.findViewById(R.id.push_id);
+        clientsList = (RecyclerView) findViewById(R.id.clientRecycler);
+        clientsList.setLayoutManager(new LinearLayoutManager(this));
 
-                name.setText(client.getName());
-                address.setText(client.getAddress());
-                pushTV.setText(client.getPushID());
+        mAdapter = new FirebaseRecyclerAdapter<Client, ClientHolder>(Client.class, R.layout.client_list_item, ClientHolder.class, databaseReference) {
+            @Override
+            protected void populateViewHolder(ClientHolder viewHolder, Client model, int position) {
+                viewHolder.setName(model.getName());
+                viewHolder.setAddress(model.getAddress());
+                viewHolder.setPushId(model.getPushID());
+
+
+                viewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position) {
+                        DialogFragment dialogFragment = new ChooseActionForClientDialog();
+
+                        TextView idTV = (TextView) view.findViewById(R.id.push_id);
+                        String pushId = idTV.getText().toString();
+
+                        TextView nameTV = (TextView) view.findViewById(R.id.client_name);
+                        String name = nameTV.getText().toString();
+
+                        TextView addressTV = (TextView) view.findViewById(R.id.client_address);
+                        String address = addressTV.getText().toString();
+
+                        Bundle args = new Bundle();
+                        args.putString(ConstantValues.CLIENT_ID_BUNDLE_KEY, pushId);
+                        args.putString(ConstantValues.CLIENT_NAME_BUNDLE_KEY, name);
+                        args.putString(ConstantValues.CLIENT_ADDRESS_BUNDLE_KEY, address);
+                        dialogFragment.setArguments(args);
+
+                        dialogFragment.show(getSupportFragmentManager(), TAG);
+                    }
+                });
 
             }
+
         };
+
+
         clientsList.setAdapter(mAdapter);
     }
 
-    private void attachOnListItemClickListener() {
-        clientsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                DialogFragment dialogFragment = new ChooseActionForClientDialog();
-
-                TextView idTV = (TextView)view.findViewById(R.id.push_id);
-                String pushId = idTV.getText().toString();
-
-                TextView nameTV = (TextView)view.findViewById(R.id.client_name);
-                String name = nameTV.getText().toString();
-
-                TextView addressTV = (TextView)view.findViewById(R.id.client_address);
-                String address = addressTV.getText().toString();
-
-                Bundle args = new Bundle();
-                args.putString(ConstantValues.CLIENT_ID_BUNDLE_KEY, pushId);
-                args.putString(ConstantValues.CLIENT_NAME_BUNDLE_KEY, name);
-                args.putString(ConstantValues.CLIENT_ADDRESS_BUNDLE_KEY, address);
-                dialogFragment.setArguments(args);
-
-                dialogFragment.show(getSupportFragmentManager(), TAG);
-            }
-        });
-    }
 
     private void handleFloatingButton() {
         FloatingActionButton floatingButton = (FloatingActionButton) findViewById(R.id.fab);
         floatingButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Bundle args = new Bundle();
-                args.putInt(ConstantValues.CHOOSE_ACTION_BUNDLE_KEY, ConstantValues.ADD_CLIENT_BUNDLE_VALUE );
+                args.putInt(ConstantValues.CHOOSE_ACTION_BUNDLE_KEY, ConstantValues.ADD_CLIENT_BUNDLE_VALUE);
                 DialogFragment dialogFragment = new AddNewOrEditClientDialog();
                 dialogFragment.setArguments(args);
                 dialogFragment.show(getSupportFragmentManager(), TAG);
@@ -103,20 +105,21 @@ public class Clients extends AppCompatActivity
         });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAdapter.cleanup();
+    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.meetings_option:
-            {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.meetings_option: {
                 Intent intent = new Intent(this, Meetings.class);
                 startActivity(intent);
                 break;
             }
-            case R.id.todays_route_option:
-            {
+            case R.id.todays_route_option: {
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 break;
@@ -126,11 +129,49 @@ public class Clients extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.clients, menu);
         return true;
     }
 
+
+    public static class ClientHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final TextView nameTV;
+        private final TextView addressTV;
+        private final TextView pushTV;
+
+        private ItemClickListener itemClickListener;
+        private String pushId;
+
+        public ClientHolder(View itemView) {
+            super(itemView);
+            nameTV = (TextView) itemView.findViewById(R.id.client_name);
+            addressTV = (TextView) itemView.findViewById(R.id.client_address);
+            pushTV = (TextView) itemView.findViewById(R.id.push_id);
+            itemView.setOnClickListener(this);
+        }
+
+
+        public void setItemClickListener(ItemClickListener itemClickListener) {
+            this.itemClickListener = itemClickListener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            itemClickListener.onClick(view, getAdapterPosition());
+        }
+
+        public void setName(String name) {
+            nameTV.setText(name);
+        }
+
+        public void setAddress(String address) {
+            addressTV.setText(address);
+        }
+
+        public void setPushId(String pushId) {
+            pushTV.setText(pushId);
+        }
+    }
 
 }
---
+++ app/src/main/java/com/bogucki/router/activities/ItemClickListener.java
@@ -0,0 +1,11 @@
+package com.bogucki.router.activities;
+
+import android.view.View;
+
+/**
+ * Created by boguc on 26.09.2017.
+ */
+
+public interface ItemClickListener {
+    void onClick(View view, int position);
+}
